c6022c073e85b694616b25b366f51eecd01b25ca
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-09-23 09:36:35 +0700

    add sql

---
+++ platform/sql/create_db.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS mobileapp_rest;
+CREATE DATABASE mobileapp_rest;
+
+SET TIMEZONE="Europe/Moscow";
+
+\c mobileapp_rest
+
+-- Create register user table
+CREATE TABLE users (
+    id INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
+    fname VARCHAR (255) NOT NULL,
+    email VARCHAR (255) NOT NULL,
+    phonenumber VARCHAR (255) NOT NULL,
+    password VARCHAR (255) NOT NULL,
+    gender VARCHAR (255) NOT NULL,
+    birthday VARCHAR NOT NULL,
+    balance INT NOT NULL
+);
+
+-- Create donates table
+CREATE TABLE transactions (
+    id INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
+    phonenumber VARCHAR (255) NOT NULL,
+    summary INT NOT NULL
+);
