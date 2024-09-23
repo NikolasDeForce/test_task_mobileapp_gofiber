7042b14de43a9e1c9f4f1529f9af0fe8c109a62b
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-09-23 09:57:25 +0700

    add iduser in struct

--- app/models/transactions.go
+++ app/models/transactions.go
@@ -6,6 +6,7 @@ import (
 
 type Transaction struct {
 	ID          int       `db:"id" json:"id"`
+	IDUser      int       `db:"iduser" json:"iduser"`
 	CreatedAt   time.Time `db:"created_at" json:"created_at"`
 	Phonenumber string    `db:"phonenumber" json:"phonenumber" validate:"required,lte=255"`
 	Summary     int       `db:"summary" json:"summary"`
